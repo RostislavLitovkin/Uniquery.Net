c53b7a01f9beddd0242f7234f759b47a21e718dd
Jaromir Prochazka <jaromir.prochazka03@gmail.com>
2023-07-10 16:48:16 +0200

    Implement Glmr data classes as children of universal data classes

--- Uniquery/Glmr/GlmrCollection.cs
+++ Uniquery/Glmr/GlmrCollection.cs
@@ -8,27 +8,15 @@ using System.Numerics;
 
 namespace Uniquery
 {
-    public class GlmrCollection
+    public class GlmrCollection : Collection
     {
         public BigInteger? BlockNumber { get; set; }
         public bool? Burned { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public String CurrentOwner { get; set; }
-        public String Id { get; set; }
         public int Max { get; set; }
         public String Issuer { get; set; }
         public String? Metadata { get; set; }
-        public RmrkMetadata? Meta { get; set; }
-        public String? Name { get; set; }
-        public List<GlmrNft> Nfts { get; set; }
         public String? Symbol { get; set; }
         public String Type { get; set; }
         public DateTime UpdatedAt { get; set; }
-
-        public override string ToString()
-        {
-            return "Glmr collection: " + Name + " by " + Issuer + " (id: " + Id + ")"
-                + "\n" + (Meta != null ? "Description : " + Meta.Description : "Metadata: " + Metadata) + "\n";
-        }
     }
 }
--- Uniquery/Glmr/GlmrNft.cs
+++ Uniquery/Glmr/GlmrNft.cs
@@ -8,25 +8,14 @@ using System.Numerics;
 
 namespace Uniquery
 {
-    public class GlmrNft
+    public class GlmrNft : Nft
     {
-        [JsonPropertyName("id")]
-        public string? Id { get; set; }
 
         [JsonPropertyName("blockNumber")]
         public BigInteger? BlockNumber { get; set; }
 
         [JsonPropertyName("burned")]
         public bool? Burned { get; set; }
-
-        [JsonPropertyName("collection")]
-        public GlmrCollection? Collection { get; set; }
-
-        [JsonPropertyName("createdAt")]
-        public DateTime? CreatedAt { get; set; }
-
-        [JsonPropertyName("currentOwner")]
-        public string? CurrentOwner { get; set; }
 
         [JsonPropertyName("events")]
         public List<GlmrEvent>? Events { get; set; }
@@ -37,14 +26,8 @@ namespace Uniquery
         [JsonPropertyName("issuer")]
         public string? Issuer { get; set; }
 
-        [JsonPropertyName("meta")]
-        public RmrkMetadata? Meta { get; set; }
-
         [JsonPropertyName("metadata")]
         public string? Metadata { get; set; }
-
-        [JsonPropertyName("name")]
-        public string? Name { get; set; }
 
         [JsonPropertyName("price")]
         public BigInteger? Price { get; set; }
@@ -60,12 +43,5 @@ namespace Uniquery
 
         [JsonPropertyName("updatedAt")]
         public DateTime? UpdatedAt { get; set; }
-
-        public override string ToString()
-        {
-            return "Glmr nft: " + Name + " - " + Sn + " owned by " + CurrentOwner + " (id: " + Id + ")"
-                + "\n" + (Meta != null ? "Description : " + Meta.Description : "Metadata: " + Metadata) + "\n" +
-                "Currently selling for: " + Price + "\n";
-        }
     }
 }
